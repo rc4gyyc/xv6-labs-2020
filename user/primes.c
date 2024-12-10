6ee0b611ae7d1f57b014abc979b5ae8c052562ad
cyan <2285738353@qq.com>
2024-12-10 22:47:25 +0800

    完成了第一个作业

--- user/primes.c
+++ user/primes.c
@@ -7,58 +7,35 @@
 const uint INT_LEN = sizeof(int);
 
 /**
- * @brief 读取左邻居的第一个数据
- * @param lpipe 左邻居的管道符
- * @param pfirst 用于存储第一个数据的地址
- * @return 如果没有数据返回-1,有数据返回0
- */
-int lpipe_first_data(int lpipe[2], int *dst)
-{
-  if (read(lpipe[RD], dst, sizeof(int)) == sizeof(int)) {
-    printf("prime %d\n", *dst);
-    return 0;
-  }
-  return -1;
-}
-
-/**
- * @brief 读取左邻居的数据，将不能被first整除的写入右邻居
- * @param lpipe 左邻居的管道符
- * @param rpipe 右邻居的管道符
- * @param first 左邻居的第一个数据
- */
-void transmit_data(int lpipe[2], int rpipe[2], int first)
-{
-  int data;
-  // 从左管道读取数据
-  while (read(lpipe[RD], &data, sizeof(int)) == sizeof(int)) {
-    // 将无法整除的数据传递入右管道
-    if (data % first)
-      write(rpipe[WR], &data, sizeof(int));
-  }
-  close(lpipe[RD]);
-  close(rpipe[WR]);
-}
-
-/**
  * @brief 寻找素数
  * @param lpipe 左邻居管道
  */
 void primes(int lpipe[2])
 {
   close(lpipe[WR]);
-  int first;
-  if (lpipe_first_data(lpipe, &first) == 0) {
-    int p[2];
-    pipe(p); // 当前的管道
-    transmit_data(lpipe, p, first);
 
-    if (fork() == 0) {
-      primes(p);    // 递归的思想，但这将在一个新的进程中调用
-    } else {
-      close(p[RD]);
-      wait(0);
-    }
+  int first;
+  if (read(lpipe[RD],  &first, INT_LEN) != INT_LEN) return;
+  printf("prime %d\n", first);
+
+  int p[2];
+  pipe(p); // 当前的管道
+
+  int data;
+  // 从左管道读取数据
+  while (read(lpipe[RD], &data, INT_LEN) == INT_LEN) {
+    // 将无法整除的数据传递入右管道
+    if (data % first)
+      write(p[WR], &data, INT_LEN);
+  }
+  close(lpipe[RD]);
+  close(p[WR]);
+
+  if (fork() == 0) {
+    primes(p);    // 递归的思想，但这将在一个新的进程中调用
+  } else {
+    close(p[RD]);
+    wait(0);
   }
   exit(0);
 }
