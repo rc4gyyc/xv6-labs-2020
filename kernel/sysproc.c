009b7f3264c420119e6ad9d8487d724c65f42b66
cyan <2285738353@qq.com>
2024-12-13 18:57:05 +0800

    完成第五个实验

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,12 +46,23 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
+  
+  struct proc* p = myproc();
+  addr = p->sz;
+  uint64 sz = p->sz;
+
+  if(n > 0) {
+    // lazy allocation
+    p->sz += n;
+  } else if(sz + n > 0) {
+    //如果 n 是负数，则对其对应的内存进行释放，但不能释放小于 0 的地址空间sz
+    sz = uvmdealloc(p->pagetable, sz, sz + n);
+    p->sz = sz;
+  } else {
+    return -1;
+  }
   // if(growproc(n) < 0)
   //   return -1;
-  
-  // lazy allocation
-  myproc()->sz += n;
   return addr;
 }
 
